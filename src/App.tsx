c86423e7d9b4fae8c8042a7c99088f1dfe869563
Alexander Korobov <dktwho@mail.ru>
2023-08-22 19:38:35 +0300

    add logic for addTaskAc, use tasksDispatch, add useReducer, add payload

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,53 @@
 import React, {useReducer, useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
-import { v1 } from 'uuid';
-import {removeTaskAC, TasksReducer} from "./reducers/tasksReducer";
+import {v1} from 'uuid';
+import {addTaskAC, removeTaskAC, TasksReducer} from "./reducers/tasksReducer";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-  // let [tasks, setTasks] = useState([
-  //   { id: v1(), title: "HTML&CSS", isDone: true },
-  //   { id: v1(), title: "JS", isDone: true },
-  //   { id: v1(), title: "ReactJS", isDone: false },
-  //   { id: v1(), title: "Rest API", isDone: false },
-  //   { id: v1(), title: "GraphQL", isDone: false },
-  // ]);
+    let [tasks, tasksDispatch] = useReducer(TasksReducer, [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false},
+        {id: v1(), title: "GraphQL", isDone: false},
+    ]);
 
-  let [tasks, tasksDispatch] = useReducer(TasksReducer, [
-    { id: v1(), title: "HTML&CSS", isDone: true },
-    { id: v1(), title: "JS", isDone: true },
-    { id: v1(), title: "ReactJS", isDone: false },
-    { id: v1(), title: "Rest API", isDone: false },
-    { id: v1(), title: "GraphQL", isDone: false },
-  ]);
+    function removeTask(id: string) {
+        tasksDispatch(removeTaskAC(id))
+    }
 
-  function removeTask(id: string) {
-    // let filteredTasks = tasks.filter(t => t.id !== id);
-    // setTasks(filteredTasks);
-    tasksDispatch(removeTaskAC(id))
-  }
+    function addTask(title: string) {
+        tasksDispatch(addTaskAC(title))
+    }
 
-  function addTask(title: string) {
-    // let task = { id: v1(), title: title, isDone: false };
-    // let newTasks = [task, ...tasks];
-    // setTasks(newTasks);
-  }
+    let [filter, setFilter] = useState<FilterValuesType>("all");
 
+    let tasksForTodolist = tasks;
 
+    if (filter === "active") {
+        tasksForTodolist = tasks.filter(t => t.isDone === false);
+    }
+    if (filter === "completed") {
+        tasksForTodolist = tasks.filter(t => t.isDone === true);
+    }
 
-  let [filter, setFilter] = useState<FilterValuesType>("all");
+    function changeFilter(value: FilterValuesType) {
+        setFilter(value);
+    }
 
-  let tasksForTodolist = tasks;
-
-  if (filter === "active") {
-    tasksForTodolist = tasks.filter(t => t.isDone === false);
-  }
-  if (filter === "completed") {
-    tasksForTodolist = tasks.filter(t => t.isDone === true);
-  }
-
-  function changeFilter(value: FilterValuesType) {
-    setFilter(value);
-  }
-
-  return (
-      <div className="App">
-        <Todolist title="What to learn"
-                  tasks={tasksForTodolist}
-                  removeTask={removeTask}
-                  changeFilter={changeFilter}
-                  addTask={addTask} />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+                      addTask={addTask}/>
+        </div>
+    );
 }
 
 export default App;
--- src/reducers/tasksReducer.ts
+++ src/reducers/tasksReducer.ts
@@ -1,24 +1,36 @@
 import {TaskType} from "../Todolist";
+import {v1} from "uuid";
 
 export const TasksReducer = (state: TaskType[], action: ActionType): TaskType[] => {
     switch (action.type) {
         case "REMOVE-TASK": {
-            return  state.filter(el => el.id !== action.payload.id)
+            return state.filter(el => el.id !== action.payload.id)
+        }
+        case "ADD-TASK": {
+            let newTask = {id: v1(), title: action.payload.title, isDone: false}
+            return [newTask, ...state]
         }
         default: {
-            return  state
+            return state
         }
     }
 }
 
 
-type ActionType = RemoveTaskACType
+type ActionType = RemoveTaskACType | AddTaskACType
 
-
-type RemoveTaskACType = ReturnType<typeof removeTaskAC >
+type RemoveTaskACType = ReturnType<typeof removeTaskAC>
 export const removeTaskAC = (id: string) => {
     return {
         type: "REMOVE-TASK",
         payload: {id}
+    } as const
+}
+
+type AddTaskACType = ReturnType<typeof addTaskAC>
+export const addTaskAC = (title: string) => {
+    return {
+        type: "ADD-TASK",
+        payload: {title}
     } as const
 }
